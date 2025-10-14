a68780d8f4876c26bf382ab7c40aab51ddaab980
Gabriel Reis <gabrielr.nogueira2000@gmail.com>
2025-10-14 00:16:19 -0300

    mostrando resultados em tela

--- interface/pages/1 - Alinhamento_Global.py
+++ interface/pages/1 - Alinhamento_Global.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from algorithm import needleman_wunsch as nw
+import numpy as np
+import pandas as pd
 
 st.title("Alinhamento global")
 st.markdown("Com needleman-wunsch")
@@ -9,12 +11,20 @@ s2 = st.text_input("S2", max_chars=100)
 
 row1_col1, row1_col2, row1_col3 = st.columns(3)
 with row1_col1:
-    match = st.number_input("Match", value=0)
+    match = st.number_input("Match", value=1)
 with row1_col2:
-    mismatch = st.number_input("Mismatch", value=0)
+    mismatch = st.number_input("Mismatch", value=-1)
 with row1_col3:
-    gap = st.number_input("Gap", value=0)
+    gap = st.number_input("Gap", value=-2)
 
 if st.button("Alinhar"):
     matrix = nw.needleman_wunsch(s1, s2, match, mismatch, gap)
-    print(matrix)
+    matrix_np = np.array(matrix)
+    columns_s2 = [f"{i}: {letter}" for i, letter in enumerate(" " + s2)] 
+    rows_s1 = [f"{i}: {letter}" for i, letter in enumerate(" " + s1)] 
+    df = pd.DataFrame(matrix_np, columns=columns_s2, index=rows_s1)
+    st.dataframe(df)
+
+    new_s1, new_s2 = nw.backtracking(s1, s2, matrix, match, mismatch, gap)
+    st.markdown(new_s1)
+    st.markdown(new_s2)
